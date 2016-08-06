2f753baf002f473c1d991cbcdca1f42623ddbdc4
Matt Odden <locke105@gmail.com>
2016-08-06 16:00:14 -0500

    Add wrapper for accessing by attributes

--- pyics/__init__.py
+++ pyics/__init__.py
@@ -31,21 +31,23 @@ class Collection(object):
 
     def __init__(self, client):
         self.client = client
+        # lookup class object from global scope, or just build one on the fly
+        def find_wrapper_cls():
+            try:
+                return globals()[self.wrapper_class_name]
+            except (AttributeError, KeyError):
+                return type(self.__class__.__name__[:-1], (Wrapper,), {})
+        self.wrapper_class = find_wrapper_cls()
 
     def list(self):
         resp = self.client.get(self.base_url + self.path)
         resp.raise_for_status()
-        return resp.json()
+        return [self.wrapper_class(item) for item in resp.json()]
 
     def create(self, *args, **kwargs):
         data_map = {}
         for k, v in kwargs.iteritems():
-            if '_' in k:
-                data_map[k.title().replace('_', '')] = v
-            else:
-                data_map[k.title()] = v
-
-        print data_map
+            data_map[unpythonize(k)] = v
 
         headers = {'Content-Type': 'application/json'}
         resp = self.client.post(
@@ -53,19 +55,19 @@ class Collection(object):
             json=data_map,
             headers=headers)
         resp.raise_for_status()
-        return resp.json()
+        return self.wrapper_class(resp.json())
 
     def show(self, obj_id):
         path = '{0}{1}/{2}'.format(self.base_url, self.path, obj_id)
         resp = self.client.get(path)
         resp.raise_for_status()
-        return resp.json()
+        return self.wrapper_class(resp.json())
 
     def delete(self, obj_id):
         path = '{0}{1}/{2}'.format(self.base_url, self.path, obj_id)
         resp = self.client.delete(path)
         resp.raise_for_status()
-        return resp
+        return resp.json()
 
 
 class Groups(Collection):
@@ -78,4 +80,45 @@ class Groups(Collection):
             normed = str(kwargs['autorecovery']).lower()
             ar_bool = normed in ('yes', 'true', 't', 1)
             kwargs['autorecovery'] = str(ar_bool)
-        super(Groups, self).create(*args, **kwargs)
+        return super(Groups, self).create(*args, **kwargs)
+
+
+class Wrapper(object):
+
+    def __init__(self, data):
+        self._body = {}
+        for k, v in data.iteritems():
+            self._body[pythonize(k)] = v
+
+    def __getattr__(self, name):
+        if name not in self._body:
+            raise AttributeError(
+                "'{0}' has no attributes '{1}'".format(self.__class__, name))
+        else:
+            return self._body[name]
+
+    def __str__(self):
+        attrbs = []
+        for k, v in self._body.iteritems():
+            attrbs.append('{0}={1}'.format(k,v))
+        stringified = '<{0} {1}>'.format(
+            self.__class__.__name__, ', '.join(attrbs))
+        return stringified
+
+    def __repr__(self):
+        return str(self)
+
+
+def pythonize(string):
+    """Transforms 'SillyCamelCase' to 'silly_camel_case'"""
+    normed = string[0].lower()
+    for char in string[1:]:
+        if char.isupper():
+            normed += '_'
+        normed += char.lower()
+    return normed
+
+
+def unpythonize(string):
+    """Transforms 'some_variable_name' to 'SomeVariableName'"""
+    return string.title().replace('_','')
